d67476d32fc089d18a0f3b89cf41c779b3b4a754
Zhenghao Xiao <zhx32@pitt.edu>
2019-12-15 18:40:47 -0500

    feat: Add scripts and plots

---
+++ plot1.R
@@ -0,0 +1,54 @@
+library(data.table)
+
+# loading dataset settings
+load_data <- function(){      
+      zipfile_name <- "household_power_consumption.zip"
+      # download zipfile
+      if (!file.exists(zipfile_name)) {
+            file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+            download.file(file_url, zipfile_name, method = "curl")
+      }
+      
+      # unzip dataset
+      file_name <- "household_power_consumption.txt"
+      if (!file.exists(file_name)) {
+            unzip(zipfile_name)
+      }
+      
+      # read the dataset from the dates 2007-02-01 and 2007-02-02
+      # use grep to find the lines contains "1/2/2007" 
+      skip_lines <- (grep("1/2/2007", readLines("household_power_consumption.txt"))[1] - 1)
+      # use the same method to get the number of rows we need to read in the original dataset
+      n_rows <- (grep("3/2/2007", readLines("household_power_consumption.txt"))[1] - 
+                 grep("1/2/2007", readLines("household_power_consumption.txt"))[1])
+      # however, if we skip rows, the column names cannot be read automatically
+      # so, we can read a small part of the original dataset to get the column names
+      hpc <- fread(file_name, nrows = 3)
+      col_names <- colnames(hpc)
+      # read the dataset 
+      hpc_df <<- fread(file_name, skip = skip_lines, nrows = n_rows, col.names = col_names)
+      
+}
+
+# reset graphical parameters before plotting
+reset_par <- function(){
+      par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1))
+}
+
+# plot settings
+plot_1 <- function(){
+      # set image 
+      png("plot1.png", width = 480, height = 480)
+      # plot 
+      with(hpc_df, hist(Global_active_power, col = "red", 
+                        main = "Global Active Power", 
+                        xlab = "Global Active Power (kilowatts)"))
+      # close the png file device
+      dev.off()
+}
+
+# workflow
+load_data()
+reset_par()
+plot_1()
+
---
+++ plot4.R
@@ -0,0 +1,68 @@
+library(data.table)
+
+# loading dataset settings
+load_data <- function(){      
+      zipfile_name <- "household_power_consumption.zip"
+      # download zipfile
+      if (!file.exists(zipfile_name)) {
+            file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+            download.file(file_url, zipfile_name, method = "curl")
+      }
+      
+      # unzip dataset
+      file_name <- "household_power_consumption.txt"
+      if (!file.exists(file_name)) {
+            unzip(zipfile_name)
+      }
+      
+      # read the dataset from the dates 2007-02-01 and 2007-02-02
+      # use grep to find the lines contains "1/2/2007" 
+      skip_lines <- (grep("1/2/2007", readLines("household_power_consumption.txt"))[1] - 1)
+      # use the same method to get the number of rows we need to read in the original dataset
+      n_rows <- (grep("3/2/2007", readLines("household_power_consumption.txt"))[1] - 
+                       grep("1/2/2007", readLines("household_power_consumption.txt"))[1])
+      # however, if we skip rows, the column names cannot be read automatically
+      # so, we can read a small part of the original dataset to get the column names
+      hpc <- fread(file_name, nrows = 3)
+      col_names <- colnames(hpc)
+      # read the dataset 
+      hpc_df <<- fread(file_name, skip = skip_lines, nrows = n_rows, col.names = col_names)
+      
+}
+
+# reset graphical parameters before plotting
+reset_par <- function(){
+      par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1))
+}
+
+# plot settings
+plot_4 <- function(){
+      # prepare data
+      date_time <- strptime(paste(hpc_df$Date, hpc_df$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+      # set file device
+      png("plot4.png", width = 480, height = 480)
+      # set plot facet
+      par(mfrow = c(2, 2))
+      # 1st plot
+      plot(date_time, hpc_df$Global_active_power, type="l", 
+           xlab="", ylab="Global Active Power", cex=0.2)
+      # 2nd plot
+      plot(date_time, hpc_df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+      # 3rd plot
+      plot(date_time, hpc_df$Sub_metering_1, type = "l", 
+           ylab = "Energy sub metering", xlab = "")
+      lines(date_time, hpc_df$Sub_metering_2, type = "l", col = "red")
+      lines(date_time, hpc_df$Sub_metering_3, type = "l", col = "blue")
+      legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+             lty=1, lwd=2.5, col=c("black", "red", "blue"))
+      # 4th plot
+      plot(date_time, hpc_df$Global_reactive_power, type="l", 
+           xlab="datetime", ylab="Global_reactive_power")
+      # close device
+      dev.off()
+}
+
+# workflow
+load_data()
+reset_par()
+plot_4()
